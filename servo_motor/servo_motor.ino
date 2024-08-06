9613f6fe08ff372bc300c9f3501416cbe56b36b5
Aadit <baggaaadit017@gmail.com>
2024-08-06 21:33:08 +0530

    kutta aadit

--- servo_motor/servo_motor.ino
+++ servo_motor/servo_motor.ino
@@ -13,10 +13,10 @@ void loop() {
   // delay(1000);
   for(int i = 0 ;i <= 180 ; i+=1){
     s1.write(i);
-    delay(10);
+    delay(5);
   }
   for(int i = 180 ;i >=0 ; i-=1){
     s1.write(i);
-    delay(10);
+    delay(5);
   }
 }
